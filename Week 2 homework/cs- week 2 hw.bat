29a375d3c921d5536b03d2deb26ef66e1c89a53d
Gary Lewman <glewman18@yahoo.com>
2021-02-08 22:47:45 -0600

    sc-week 2 homework

---
+++ Week 2 homework/cs- week 2 hw.bat
@@ -0,0 +1,14 @@
+let pizzaPlace = "Gary's Pizza Shack";
+let numberOfToppings = 5;
+
+typeof "Gary's Pizza Shack";
+typeof 5;
+
+`"Welcome to ${pizzaPlace}, we may only have ${numberOfToppings} toppings, but they are delicious."`;
+if (numberOfToppings < 10) {
+  console.log("Quality, not Quantity");
+};
+
+for(let i = 2; i <= 10; i+2) {
+console.log(i);
+}
