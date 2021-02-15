a72c2f0146e61fcf40b7408bf8d55e57842c3677
Gary Lewman <glewman18@yahoo.com>
2021-02-14 18:07:11 -0600

    Pizza place HW Week 3

--- Week 2 homework/cs- week 2 hw.bat
+++ Week 2 homework/cs- week 2 hw.bat
@@ -4,11 +4,11 @@ let numberOfToppings = 5;
 typeof "Gary's Pizza Shack";
 typeof 5;
 
-`"Welcome to ${pizzaPlace}, we may only have ${numberOfToppings} toppings, but they are delicious."`;
+console.log(`"Welcome to ${pizzaPlace}, we may only have ${numberOfToppings} toppings, but they are delicious."`);
 if (numberOfToppings < 10) {
   console.log("Quality, not Quantity");
 };
 
-for(let i = 2; i <= 10; i+2) {
+for(let i = 2; i <= 10; i+=2) {
 console.log(i);
 }
